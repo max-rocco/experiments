b42784304a0cb6aea39cafd8ff6dd97e8f4bfe67
Max Rocco <76984797+max-rocco@users.noreply.github.com>
2021-02-21 18:56:33 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,3 @@
-function logger() {
-  const { NODE_ENV } = process.env;
-  const isLogEnvironment = NODE_ENV == 'development' || NODE_ENV == 'test';
-
-  return function(message, ...moreMessages) {
-    if(!isLogEnvironment) return;
-    return console.log(message, ...moreMessages);
-  }
-}
+(function main(message: string) {
+  console.log(message);
+})('Hello, world! ⚡');
